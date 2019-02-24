1cb62660872739e432217cc783cd6e6114093bf4
Mark Chandler <mark@chanchan.io>
2019-02-24 10:28:13 -0500

    chore: add rollup terser minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
+import {terser} from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 export default [
   {
     input: 'src/index.js',
     external: ['react-scripts/config/paths'],
+    plugins: [terser()],
     output: [{file: pkg.main, format: 'cjs'}],
   },
 ]
